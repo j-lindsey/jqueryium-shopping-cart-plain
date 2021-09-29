dfe1dddd8bd56edb3865522713873dbeecd28db1
j-lindsey <j-lindsey@users.noreply.github.com>
2021-09-29 15:31:36 -0600

    added total price calculation and total item price calculation

---
+++ js/application.js
@@ -0,0 +1,39 @@
+//Function to calculate Total Price of items
+let updateTotalPrice = function () {
+    $('tbody tr').each(function (i, ele) {
+        let pricePerUnit = parseFloat(($(ele).children('.pricePerUnit').text()).substring(1));
+        let qty = parseFloat($(ele).find('.qty input').val());
+        let totalPrice = pricePerUnit * qty;
+        $(ele).children('.totalItemPrice').html('$' + totalPrice.toFixed(2));
+    });
+}
+
+
+$(document).ready(function () {
+    updateTotalPrice();
+
+    $(document).on('input', '.qty input', function () {
+        updateTotalPrice();
+    });
+
+    $('#addItem').on('submit', function (event) {
+        event.preventDefault();
+        let item = $(this).children('[name=item]').val();
+        let pricePerUnit = parseFloat($(this).children('[name=pricePerUnit]').val()).toFixed(2);
+        let qty = $(this).children('[name=qty]').val();
+        
+        $('tbody').append(`<tr><td class="item">${item}</td><td class="pricePerUnit">$${pricePerUnit}</td><td class="qty"><input value=${qty} /></td><td class="cancel"><button>Cancel</button></td><td class="totalItemPrice"></td></tr>`);
+        
+        updateTotalPrice();
+    });
+
+    $('.btn.calculatePrice').on('click', function () {
+        let sum = 0;
+        $('tbody tr').each(function (i, ele) {
+            sum += parseFloat(($(ele).children('.totalItemPrice').text()).substring(1));
+        });
+        $('#totalPrice').html('$' + sum.toFixed(2));
+    })
+});
+
+
